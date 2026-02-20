afb6d3f63bb76a3f6c28c28e6925eff065bf7873
ivansosabz <ivansosabz@gmail.com>
2026-02-20 17:28:24 -0300

    clients back

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = (
+        "nombres",
+        "apellidos",
+        "numero_identificacion",
+        "tipo_identificacion",
+        "estado",
+        "fecha_creacion",
+    )
+
+    list_filter = ("estado", "tipo_identificacion")
+    search_fields = ("nombres", "apellidos", "numero_identificacion")
--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,44 @@
+import uuid
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Client(models.Model):
+
+    class TipoIdentificacion(models.TextChoices):
+        CEDULA = "cedula", "Cédula"
+        RUC = "ruc", "RUC"
+        PASAPORTE = "pasaporte", "Pasaporte"
+
+    class EstadoCliente(models.TextChoices):
+        ACTIVO = "activo", "Activo"
+        INACTIVO = "inactivo", "Inactivo"
+        PENDIENTE = "pendiente_verificacion", "Pendiente Verificación"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    tipo_identificacion = models.CharField(
+        max_length=20, choices=TipoIdentificacion.choices
+    )
+
+    numero_identificacion = models.CharField(max_length=30, unique=True)
+
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+
+    fecha_nacimiento = models.DateField()
+
+    nacionalidad = models.CharField(max_length=50, default="paraguaya")
+
+    telefono = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    fecha_creacion = models.DateTimeField(default=timezone.now)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+
+    estado = models.CharField(
+        max_length=30, choices=EstadoCliente.choices, default=EstadoCliente.ACTIVO
+    )
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos} - {self.numero_identificacion}"
